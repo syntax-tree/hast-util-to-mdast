1de75a865561560764511cd490bb38db2c350bd4
Titus Wormer <tituswormer@gmail.com>
2017-02-02 19:49:39 +0100

    Add several ignored elements (`title`, `script`, etc)

--- handlers/index.js
+++ handlers/index.js
@@ -4,6 +4,9 @@ exports.root = require('./root');
 exports.text = require('./text');
 exports.comment = require('./comment');
 
+exports.head = exports.math = exports.script = exports.style = exports.svg =
+  exports.template = exports.title = ignore;
+
 exports.strong = exports.b = require('./strong');
 exports.em = exports.i = require('./emphasis');
 exports.del = exports.s = exports.strike = require('./delete');
@@ -19,3 +22,5 @@ exports.h1 = exports.h2 = exports.h3 =
   exports.h4 = exports.h5 = exports.h6 = require('./heading');
 
 exports.a = require('./link');
+
+function ignore() {}
