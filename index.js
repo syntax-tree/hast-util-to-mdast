a404d8d12abeaeae083368f8b5394ac2f998da32
Titus Wormer <tituswormer@gmail.com>
2017-07-05 19:46:42 +0200

    Fix modifying of constant functions

--- index.js
+++ index.js
@@ -7,39 +7,40 @@ var xtend = require('xtend');
 var one = require('./one');
 var handlers = require('./handlers');
 
-h.augment = augment;
-
 function toMDAST(tree, options) {
-  options = options || {};
-  h.handlers = xtend(handlers, options.handlers || {});
+  var settings = options || {};
+
+  h.handlers = xtend(handlers, settings.handlers || {});
+  h.augment = augment;
+
   return one(h, minify(tree), null);
-}
 
-function h(node, type, props, children) {
-  var result;
+  function h(node, type, props, children) {
+    var result;
 
-  if (!children && ((typeof props === 'object' && 'length' in props) || typeof props === 'string')) {
-    children = props;
-    props = {};
+    if (!children && ((typeof props === 'object' && 'length' in props) || typeof props === 'string')) {
+      children = props;
+      props = {};
+    }
+
+    result = xtend({type: type}, props);
+
+    if (typeof children === 'string') {
+      result.value = children;
+    } else if (children) {
+      result.children = children;
+    }
+
+    return augment(node, result);
   }
 
-  result = xtend({type: type}, props);
+  /* `right` is the finalized MDAST node,
+   * created from `left`, a HAST node */
+  function augment(left, right) {
+    if (left.position) {
+      right.position = left.position;
+    }
 
-  if (typeof children === 'string') {
-    result.value = children;
-  } else if (children) {
-    result.children = children;
+    return right;
   }
-
-  return augment(node, result);
-}
-
-/* `right` is the finalized MDAST node,
- * created from `left`, a HAST node */
-function augment(left, right) {
-  if (left.position) {
-    right.position = left.position;
-  }
-
-  return right;
 }
