1f2dfb55bb4a31724ecca08b7e18650078949875
Titus Wormer <tituswormer@gmail.com>
2023-01-12 10:30:01 +0100

    Remove non-elements from `handlers`
    Previously, nodes could be handled by their `type`.
    In combination with elements by their `tagName`.
    This could result in bugs if for example `<root>`, `<comment>`, or
    `<text>` or so was used.
    
    Now, `handlers` only supports elements by their tag name, and a new
    `nodeHandlers` exists for (custom) nodes.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 /**
  * @typedef {import('./lib/state.js').State} State
  * @typedef {import('./lib/types.js').Handle} Handle
+ * @typedef {import('./lib/types.js').NodeHandle} NodeHandle
  * @typedef {import('./lib/types.js').Options} Options
  */
 
-export {defaultHandlers, toMdast} from './lib/index.js'
+export {toMdast} from './lib/index.js'
+
+export {
+  handlers as defaultHandlers,
+  nodeHandlers as defaultNodeHandlers
+} from './lib/handlers/index.js'
