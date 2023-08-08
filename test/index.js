211e0536ad2a5f4c52414705b21d38cb1f745a7a
Titus Wormer <tituswormer@gmail.com>
2023-08-08 17:46:47 +0200

    Change to use `exports`

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 /**
- * @typedef {import('../index.js').Options} Options
+ * @typedef {import('hast-util-to-mdast').Options} Options
  */
 
 /**
@@ -17,6 +17,7 @@ import process from 'node:process'
 import test from 'node:test'
 import {h} from 'hastscript'
 import {fromHtml} from 'hast-util-from-html'
+import {toMdast} from 'hast-util-to-mdast'
 import {isHidden} from 'is-hidden'
 import {assert as mdastAssert} from 'mdast-util-assert'
 import {fromMarkdown} from 'mdast-util-from-markdown'
@@ -25,11 +26,10 @@ import {toMarkdown} from 'mdast-util-to-markdown'
 import {gfm} from 'micromark-extension-gfm'
 import {u} from 'unist-builder'
 import {removePosition} from 'unist-util-remove-position'
-import {toMdast} from '../index.js'
 
 test('core', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('../index.js')).sort(), [
+    assert.deepEqual(Object.keys(await import('hast-util-to-mdast')).sort(), [
       'defaultHandlers',
       'defaultNodeHandlers',
       'toMdast'
